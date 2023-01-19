66295cf0eb19b68ab6c644c2a9d5d3a02a47424c
Onigiri7760 <123040130+Onigiri7760@users.noreply.github.com>
2023-01-18 23:37:50 -0700

    Add files via upload

---
+++ CIS188_MaierJ_Lab12Part2.ps1
@@ -0,0 +1,8 @@
+﻿#CIS188 Module 12 Lab 12 Part 2
+#Powershell Script
+#Author: Jennifer Maier
+
+#This program takes the first name, last name, and phone numbers in the employees.csv file and creates and move it into a new phone.csv file.
+
+Import-Csv -Path OneDrive\Documents\powershell\employees.csv | Select-Object -Property fname, lname, phone | Export-Csv -Path OneDrive\Documents\powershell\phone.csv
+Import-Csv -Path OneDrive\Documents\powershell\phone.csv
